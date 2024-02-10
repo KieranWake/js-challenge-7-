bd04cd7fca7b822aaaebe7f3358a6604468c7fdf
Kieran Wake <kieranwake1234@gmail.com>
2024-02-10 11:12:14 +0000

    pseudocoding

---
+++ script.js
@@ -0,0 +1,15 @@
+function absolute(n) {
+
+	
+}
+
+//What is the abs function? 
+
+//Math.abs() function returns the absolute value of a number. 
+
+// The function is particularly useful for converting negative numbers into positive numbers -
+// Taking this into account. To recreate the math.abs function, we can times a negative -
+// by a negative to equal a positive for the same functionality. Lets pseudocode! 
+
+//declare a let variable which will store our data. 
+//
